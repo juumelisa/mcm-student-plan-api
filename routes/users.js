2bc13abba00a1952cb356f4d0be7f4ed1d352889
juumelisa <jumelisa09@gmail.com>
2023-03-09 08:18:35 +0700

    feat: eslint

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.json({
+    error: 0,
+    message: 'Hello World'
+  });
 });
 
 module.exports = router;
