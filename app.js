cb1da444adca60038b8732bf0836e85548af9c19
juumelisa <jumelisa09@gmail.com>
2023-03-09 10:48:30 +0700

    feat: get all subject

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var logger = require('morgan');
 const sequelize = require('./helpers/sequelize');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var subjectRouter = require('./routes/subject');
 var studentRouter = require('./routes/student');
 const students = require('./seeders/student');
+const subjects = require('./seeders/subject');
 
 var app = express();
 require('dotenv').config();
@@ -23,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/student', studentRouter);
+app.use('/subject', subjectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,6 +46,7 @@ app.use(function(err, req, res) {
 app.listen(8000, async () => {
   await sequelize.sync();
   await students();
+  await subjects();
 });
 
 module.exports = app;
